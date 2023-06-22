241e01fedd1cff4c4455476c03e20ac556c9387c
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-06-22 19:04:55 +0200

    Update Mr.X asks for help.cpp

--- Zahra_Binary/Mr.X asks for help.cpp
+++ Zahra_Binary/Mr.X asks for help.cpp
@@ -1,10 +1,10 @@
 //PL : https://codeforces.com/group/SbXdq02gzF/contest/431638/problem/B
 /*
 For each test case, it checks from element 0 to high (which is the cube root of the number entered by the user) and compares it to the sum
- (which is the addition of the low and high power 3)
-      >> If sum > x, then we need to decrease the hi value and looping again
-      >> If sum < x, then we need to increase the lo value and looping again 
-      >> until it equals the sum value, then the equation  a^3+b^3=x is achieved and returns true to the main function and prints YES
+ (which is the addition of the low power 3 and high power 3)
+      - If sum > x, then we need to decrease the hi value and looping again
+      - If sum < x, then we need to increase the lo value and looping again 
+      - Until it equals the sum value, then the equation  a^3+b^3=x is achieved and returns true to the main function and prints YES
           otherwise, No
 */
 #include <iostream>
