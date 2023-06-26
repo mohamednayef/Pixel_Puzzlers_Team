e51c607c5fda9e20b8cefa8dc5e2ea8f2b78d264
mohamednayef <122391287+mohamednayef@users.noreply.github.com>
2023-06-26 09:06:14 +0300

    Create SumofConsecutiveOddNumbers.cpp

---
+++ Nayef_Loops_Assuit/SumofConsecutiveOddNumbers.cpp
@@ -0,0 +1,37 @@
+//  Problem link : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/S
+//
+//  Created by   : Mohamed Nayef on 13/06/2023.
+//
+ 
+#include <iostream>
+#include <deque>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+ 
+    
+    int tests;
+    cin >> tests;
+    while(tests--)
+    {
+        int x, y, sum=0;
+        cin >> x >> y;
+        int minimum = min(x,y);
+        int maximum = max(x,y);
+        
+        for(int i=minimum+1; i<maximum; i++)
+        {
+            if(i%2 == 1)
+                sum += i;
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
